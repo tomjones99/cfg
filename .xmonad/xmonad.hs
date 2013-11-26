e3fc89df70125af854a41a21ad91d298ef02a2fc
galan <galan969@gmail.com>
2013-11-25 21:57:29 -0800

    Arch: xmonad cfg

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,12 +2,17 @@ import XMonad
 import XMonad.Actions.SpawnOn
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.SimpleFloat
 
 -- main
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
 -- status bar
 myBar = "xmobar"
+
+-- default terminal
+myTerminal = "urxvt"
 
 -- PP
 myPP = xmobarPP { ppCurrent = xmobarColor "#ff0000" "" . wrap "<" ">"
@@ -21,19 +26,34 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 -- move applications to a given workspace
 myManageHook = composeAll
     [ className =? "Chromium" --> doShift "2:web"
+    , className =? "weechat" --> doShift "3:chat"
     , manageDocks
     ]
 
 -- workspaces
-myWorkspaces = ["1:dev", "2:web", "3:mail", "4", "5", "6", "7", "8", "9" ]
+myWorkspaces = ["1:dev", "2:web", "3:chat", "4:mail", "5", "6", "7", "8", "9" ]
+
+-- layouts
+defaultLayouts = tiled ||| Mirror tiled ||| simpleFloat ||| Full
+    where
+        tiled = Tall nmaster delta ratio
+        nmaster = 1
+        ratio = 1/2
+        delta = 3/100
+
+myLayouts = defaultLayouts
 
 -- launch programs on startup
 myStartupHook = do
+    spawnOn "1:dev" "urxvt"
     spawnOn "2:web" "chromium"
+    --spawnOn "3:chat" "urxvt -e weechat-curses"
 
 -- config
 myConfig = defaultConfig
-    { workspaces = myWorkspaces
+    { terminal = myTerminal
+    , workspaces = myWorkspaces
+    , layoutHook = myLayouts
     , manageHook = myManageHook <+> manageHook defaultConfig
     , startupHook = myStartupHook
     }
